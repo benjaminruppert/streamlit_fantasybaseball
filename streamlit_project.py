ecd82de7bd218e977776493cb48ab33d2b52a060
benjaminruppert <benjamin.ruppert13@gmail.com>
2025-02-08 20:40:25 -0800

    Add files via upload

--- streamlit_project.py
+++ streamlit_project.py
@@ -1,119 +1,53 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-
-finalinnine = 0
-finalinnings = 0
-
-##need to remove this
-opp_Ks = 10
-my_Ks = 5
-
-
-st.title("Should you stream that pitcher? ")
+st.title("Should you stream that pitcher?")
 st.subheader("Enter below and find out!")
 
+st.subheader("First, let's check ERA and WHIP categories:")
 
-st.subheader("First, let's check to see if your ERA and WHIP goals are in reach")
-opp_era = st.number_input("Enter your oppenent's ERA")
-opp_whip = st.number_input("Enter your oppenent's WHIP")
+opp_era = st.number_input("Enter your opponent's ERA")
+opp_whip = st.number_input("Enter your opponent's WHIP")
 my_era = st.number_input("Enter your ERA")
 my_whip = st.number_input("Enter your WHIP")
 my_ip = st.number_input("Enter your innings pitched")
 
-## make this into a function!
-if opp_era > my_era:
-    st.subheader("You are already ahead in ERA")
-if opp_whip > my_whip:
-    st.subheader("You are already ahead in WHIP")
+def check_stats(my_era, opp_era, my_whip, opp_whip, my_ip):
+    results = {}
     
-
-##calc to get earned runs from era
-my_er = my_era*my_ip/9
-##calc to get additional walks+hits for whip
-addwalkshits = opp_whip*(my_ip + 9)
-##calc to get K difference
-kdiff = opp_Ks - my_Ks
-
-##calc to return for era
-if opp_era != 0:
-    #final = 9*my_er/opp_era
-    finalinnings = ((my_era * my_ip) - (opp_era * my_ip)) / (9 * opp_era - my_era)
-    finalinnine = 9/finalinnings
-    finalksinnine = opp_Ks/9
-
-##ERA again...
-#if opp_era != 0:
-    #finalinnine = finalinnings/9
-
-
-
-##calc to return for whip
-finalwhip = addwalkshits/(my_ip + 9)
-
-
+    if my_era < opp_era:
+        results['ERA'] = "You're winning already"
+    else:
+        results['ERA'] = compute_era_scenario(my_era, opp_era, my_ip)
     
+    if my_whip < opp_whip:
+        results['WHIP'] = "You're winning already"
+    else:
+        results['WHIP'] = compute_whip_scenario(my_whip, opp_whip)
+    
+    return results
+
+def compute_era_scenario(my_era, opp_era, my_ip):
+    if opp_era == 0:
+        return "Opponent's ERA is zero, computation not possible."
+    
+    my_er = my_era * my_ip / 9  # Calculate earned runs
+    finalinnings = 9 * my_er / opp_era
+    er_freeinnings = finalinnings - my_ip
+    
+    return er_freeinnings
+
+def compute_whip_scenario(my_whip, opp_whip):
+
+    mywalkshits = my_whip * my_ip
+    finalinnings = (mywalkshits + opp_whip) - my_ip
+    
+    return finalinnings
+
+# Perform computation
+finaloutput = check_stats(my_era, opp_era, my_whip, opp_whip, my_ip)
 
 
+st.subheader("Shutout Innings Needed:")
 
-##get data for scatter
-df = pd.read_csv("SQLdailypitching.csv")
-
-
-##add calculated columns from SQL
-forera = df[["ER", "SO", "IP"]]
-forera["K/9"] = 9*(forera["SO"] / forera["IP"]) 
-forera["ERA"] = 9*(forera["ER"] / forera["IP"])
-
-# Add the new row using loc (only K/9 and ERA will be filled out). ORDER MATTERS!
-forera.loc[len(forera)] = [np.nan, np.nan, np.nan, kdiff, finalinnine]
-
-
-# Output
-st.subheader("Here's how many run free innings you need")
-my_er
-finalinnine
-finalinnings
-st.subheader("Here's the WHIP you need from your streamers (scaled to 9IP)")
-finalwhip
-
-
-
-
-st.subheader("Here's a scatterchart for the season and your data point plotted in yellow? Will it happen?")
-
-# Function to apply color based on condition
-def color_point(val, column):
-    if (column == 'ERA' and val == finalinnine) or (column == 'K/9' and val == kdiff):
-        return 'background-color: red'
-    return ''
-
-
-
-# Getting colors for scatterplot
-def color_point(val, column):
-    if (column == 'ERA' and val == finalinnine) or (column == 'K/9' and val == kdiff):
-        return 'background-color: red'
-    return ''
-
-# Apply styling
-styled_df = forera.style.applymap(lambda val: color_point(val, 'ERA'), subset=['ERA']) \
-                        .applymap(lambda val: color_point(val, 'K/9'), subset=['K/9'])
-
-# Adding colors back as a column
-forera['color'] = forera.apply(lambda row: 'red' if (row['ERA'] == finalinnine and row['K/9'] == kdiff) else 'blue', axis=1)
-# Adding for different sized circles
-forera['frequency'] = forera.groupby(['ERA', 'K/9']).transform('size')
-
-
-
-# Display the scatter chart with colors
-st.scatter_chart(forera,x = "ERA",y = "K/9", color = "color", size = 'frequency')
-
-
-
-
-
-
-
+# Display results
+st.write(finaloutput)
