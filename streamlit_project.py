3884fa35d94ed90c729277391bcdfa2b34c0d8ab
benjaminruppert <benjamin.ruppert13@gmail.com>
2025-06-18 22:43:39 -0700

    Update streamlit_project.py

--- streamlit_project.py
+++ streamlit_project.py
@@ -2,6 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from streamlit_gsheets import GSheetsConnection
+
+## G sheet connection
+conn = st.connection("gsheets",type=GSheetsConnection)
+
 
 st.title("Pitching Ratio Stat Calculator") 
 
